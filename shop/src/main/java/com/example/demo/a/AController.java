cfa0e3cf52ba472f5198587a4c5ce33c0542fb12
Mikyung Jeon <mikyung22e@naver.com>
2018-11-21 15:31:57 +0900

    a add

--- shop/src/main/java/com/example/demo/a/AController.java
+++ shop/src/main/java/com/example/demo/a/AController.java
@@ -1,5 +1,11 @@
 package com.example.demo.a;
 
-public class AController {
+import org.springframework.stereotype.Controller;
 
+@Controller	
+public class AController {
+	public String start() {
+		System.out.println();
+		return "a/index";
+	}
 }
